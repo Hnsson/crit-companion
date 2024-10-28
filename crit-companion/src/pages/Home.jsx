b4465b189ec3ec37c4f2126ce46630ed9f0a68dc
Hnsson <emhs21@student.bth.se>
2024-10-28 02:08:03 +0100

    Changed home page design

--- crit-companion/src/pages/Home.jsx
+++ crit-companion/src/pages/Home.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
+import "./Home.css";
+
 function Home() {
-  return <h2>Welcome to the Crit-Companion Home Page!</h2>;
+  return (
+    <main className="home-container">
+      <header>
+        <h1>Crit-Companion</h1>
+      </header>
+      <div className="divider"></div>
+      <section className="text">
+        <p>Your all-in-one tool to craft, manage, and explore Dungeons & Dragons characters and creations. Unleash your imagination:</p>
+        <a href="/about">Discover more</a>
+      </section>
+    </main>
+  )
 }
 
 export default Home;
